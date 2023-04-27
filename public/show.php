e315aa09abbb69e4ea00b062a8f328d17ab98915
easygithdev <easygithdev@gmail.com>
2023-04-27 17:45:40 +0200

    fix show

--- public/show.php
+++ public/show.php
@@ -12,6 +12,14 @@ $config = require __DIR__ . '/../config/conf.php';
 
 header('Content-Type: image/png');
 
-$image = $config['downloadDir'] . '/' . $_GET['filename'];
+$filename = filter_input(INPUT_GET, 'filename', FILTER_SANITIZE_SPECIAL_CHARS);
 
-readfile($image);
+if (empty($filename)) {
+    $fp = fopen("https://www.php.net/images/logos/php-logo-bigger.png", 'rb');
+
+    // dump the picture and stop the script
+    fpassthru($fp);
+} else {
+    $image = $config['downloadDir'] . '/' . $_GET['filename'];
+    readfile($image);
+}
